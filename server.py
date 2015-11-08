ed635d18b8612b5d657fd9928d4781d6bb3119f8
Allen Czerwinski <allen.czerwinski@gmail.com>
2015-11-07 21:40:22 -0800

    create functionality and change names of images

--- server.py
+++ server.py
@@ -1,8 +1,30 @@
-from flask import Flask, render_template, request, redirect, session, flash, send_from_directory
-import random
+from flask import Flask, render_template, request, redirect, session, flash, send_from_directory, url_for
 app = Flask(__name__)
 app.secret_key="SecreKeyMePls"
-@app.route('/')
+@app.route('/', methods = ['GET'])
 def index():
-	return render_template("index.html")
+	return render_template("/index.html")
+@app.route("/ninja")
+def ninja():
+	turtle = "four_turtles"
+	turtle_name = "all four turtles"
+	return render_template("ninja.html", turtle_name=turtle_name, turtle=turtle)
+@app.route('/ninja/<turtle>', methods=['GET'])
+def ninja_color(turtle):
+	if turtle == "purple":
+		turtle_name = "Donatello"
+		return render_template("/ninja.html", turtle_name=turtle_name, turtle=turtle)
+	elif turtle == "red":
+		turtle_name = "Raphael"
+		return render_template("/ninja.html", turtle_name=turtle_name, turtle=turtle)
+	elif turtle == "orange":
+		turtle_name = "Michelangelo"
+		return render_template("/ninja.html", turtle_name=turtle_name, turtle=turtle)
+	elif turtle == "blue":
+		turtle_name = "Leonardo"
+		return render_template("/ninja.html", turtle_name=turtle_name, turtle=turtle)
+	else:
+		turtle = "meganfox"
+		turtle_name = "Megan Fox"
+		return render_template("/ninja.html", turtle_name=turtle_name, turtle=turtle)
 app.run(debug=True)
